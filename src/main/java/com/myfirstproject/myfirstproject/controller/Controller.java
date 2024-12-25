2bd495797af62167bd0c0059bc4cb4f5634af963
ksaikumar <ksaikumar5148@gmail.com>
2024-12-25 21:32:02 +0530

    backend first commit

---
+++ src/main/java/com/myfirstproject/myfirstproject/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.myfirstproject.myfirstproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+	
+	
+	@GetMapping
+	public String hello() {
+		return "helloworld";
+	}
+
+}
